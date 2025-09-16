f3143d3819262411604b8c0ec3dd80647cfa213a
BSALINAS <bernadette.salinas2002@gmail.com>
2025-09-16 10:30:02 +0800

    add axios for get, update and delete note

--- src/Controller/NotesController.js
+++ src/Controller/NotesController.js
@@ -10,13 +10,27 @@ export function postNotes(params) {
     });
 }
 
+export function updateNotes(params) {
+  return axios.put(baseURL + "/api/Notes", params)
+    .then(res => res.data)
+    .catch(err => {
+      console.error('Error:', err);
+      throw err;
+    });
+}
+
 export function getNotes(params) {
-   console.log('paaramas before getNotes:', params);
- axios.get(baseURL + "/api/Notes",  { params})
-    .then(res => {
-      console.log('res dataa:', res.data[0]); // or res.data if you only want the payload
-      return res.data[0];
-    })
+  return axios.get(baseURL + "/api/Notes", { params })
+    .then(res =>  res.data[0])
+    .catch(err => {
+      console.error('Error:', err);
+      throw err;
+    });
+}
+
+export function deleteNotes(params) {
+  return axios.delete(baseURL + "/api/Notes",  {params} )
+    .then(res =>  res.data[0])
     .catch(err => {
       console.error('Error:', err);
       throw err;
