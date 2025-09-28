9e4cbce155f7027f8eda0b1184cdb52d3b493764
BSALINAS <bernadette.salinas2002@gmail.com>
2025-09-28 11:54:16 +0800

    add getdatetime in commonUtils

--- src/mainslice/commonUtils.js
+++ src/mainslice/commonUtils.js
@@ -1,3 +1,13 @@
 export function getDateOnly(date = new Date()) {
-  return date.toISOString().split('T')[0];
+  return date.toISOString().split("T")[0];
+}
+export function getDateTime(date = new Date()) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
