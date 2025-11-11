d995d29f58a792558fc2065fcf0b8ca168a6efb2
BSALINAS <bernadette.salinas2002@gmail.com>
2025-11-11 17:29:32 +0800

    update notes api endpoints for clarity

--- src/Controller/NotesController.js
+++ src/Controller/NotesController.js
@@ -1,39 +1,42 @@
 import axios from "axios";
-const baseURL = 'https://localhost:7270';
+const baseURL = "https://localhost:7270";
 
 export function postNotes(params) {
-  return axios.post(baseURL + "/api/Notes", params)
-    .then(res => res.data)
-    .catch(err => {
-      console.error('Error:', err);
+  return axios
+    .post(baseURL + "/api/Notes/PostNote", params)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error("Error:", err);
       throw err;
     });
 }
 
 export function updateNotes(params) {
-  return axios.put(baseURL + "/api/Notes", params)
-    .then(res => res.data)
-    .catch(err => {
-      console.error('Error:', err);
+  return axios
+    .put(baseURL + "/api/Notes/UpdateNote", params)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error("Error:", err);
       throw err;
     });
 }
 
 export function getNotes(params) {
-  return axios.get(baseURL + "/api/Notes", { params })
-    .then(res =>  res.data[0])
-    .catch(err => {
-      console.error('Error:', err);
+  return axios
+    .get(baseURL + "/api/Notes/GetNote", { params })
+    .then((res) => res.data[0])
+    .catch((err) => {
+      console.error("Error:", err);
       throw err;
     });
 }
 
 export function deleteNotes(params) {
-  return axios.delete(baseURL + "/api/Notes",  {params} )
-    .then(res =>  res.data[0])
-    .catch(err => {
-      console.error('Error:', err);
+  return axios
+    .delete(baseURL + "/api/Notes/DeleteNote", { params })
+    .then((res) => res.data[0])
+    .catch((err) => {
+      console.error("Error:", err);
       throw err;
     });
 }
-
